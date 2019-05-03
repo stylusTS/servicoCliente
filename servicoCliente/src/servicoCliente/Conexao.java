cab385dc55fcbe0bffda612d6572cba4c3c33a86
Paulo <tecnostylus@gmail.com>
2019-05-03 17:34:40 -0300

    servico operacional para mysql

--- servicoCliente/src/servicoCliente/Conexao.java
+++ servicoCliente/src/servicoCliente/Conexao.java
@@ -11,7 +11,7 @@ public class Conexao {
 	public String retorno() throws SQLException, ClassNotFoundException {
 			
 	
-	   // Class.forName("com.mysql.jdbc.Driver"); // "carregar" o driver, de tal maneira que o Java saiba que ele existe, obrigatório!!
+	    Class.forName("com.mysql.jdbc.Driver"); // "carregar" o driver, de tal maneira que o Java saiba que ele existe, obrigatório!!
 		Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/bdservico", "root", "1234");
 		Statement statement = connection.createStatement();	
 		
